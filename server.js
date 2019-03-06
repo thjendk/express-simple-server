47c01cb939fde8ab356b89b2459ea23df2dd8ccd
thomasjensen1194 <44089656+thomasjensen1194@users.noreply.github.com>
2019-03-06 22:47:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+// Serve index.js
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.listen(port);
